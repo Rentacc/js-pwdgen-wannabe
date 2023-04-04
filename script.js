fc5f06801cf845be1c79c22d1182016672fa5bed
Rentacc <124709345+Rentacc@users.noreply.github.com>
2023-04-05 01:56:44 +0200

    add prompts

---
+++ script.js
@@ -0,0 +1,19 @@
+// Chiedi all’utente il suo nome,poi chiedi il suo cognome,poi chiedi il suo colore preferito .Infine scrivi sulla pagina nomecognomecolorepreferito23 
+
+
+
+let mioNome;
+mioNome = 'Inserisci il tuo nome:';
+const text = prompt(mioNome);
+let mioCognome;
+mioCognome = 'Inserisci il tuo cognome:';
+const text2 = prompt(mioCognome);
+let mioColore;
+mioColore = 'Inserisci il tuo colore preferito:';
+const text3 = prompt(mioColore);
+
+let text4 = '23';
+
+document.writeln(text + text2 + text3 + text4);
+
+
